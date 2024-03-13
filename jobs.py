741238fb67b551a36098ae92dff718c076474531
mamba <isaiahntina47@gmail.com>
2024-03-13 18:23:24 +0000

    Add job scraping functionality to jobs.py

---
+++ jobs.py
@@ -0,0 +1,17 @@
+import csv
+import json
+from jobspy import scrape_jobs
+
+jobs = scrape_jobs(
+    # site_name=["indeed", "linkedin", "zip_recruiter", "glassdoor"],
+    site_name=["linkedin"],
+    search_term="software engineer",
+    location="Dallas, TX",
+    results_wanted=20,
+    hours_old=72, # (only Linkedin/Indeed is hour specific, others round up to days old)
+    country_indeed='USA'  # only needed for indeed / glassdoor
+)
+print(f"Found {len(jobs)} jobs")
+print(jobs.head())
+
+jobs.to_csv("jobs.csv", quoting=csv.QUOTE_NONNUMERIC, escapechar="\\", index=False)
