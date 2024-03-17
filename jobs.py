9202426dc9e0316f1974e6ba4d4fbe14d729b25f
mamba <isaiahntina47@gmail.com>
2024-03-17 17:42:07 +0000

    Add subprocess to run jsonify.py

--- jobs.py
+++ jobs.py
@@ -1,13 +1,14 @@
 import csv
 import json
 from jobspy import scrape_jobs
+import subprocess
 
 jobs = scrape_jobs(
     # site_name=["indeed", "linkedin", "zip_recruiter", "glassdoor"],
     site_name=["linkedin"],
     search_term="software engineer",
     location="Dallas, TX",
-    results_wanted=20,
+    results_wanted= 10,
     hours_old=72, # (only Linkedin/Indeed is hour specific, others round up to days old)
     country_indeed='USA'  # only needed for indeed / glassdoor
 )
@@ -15,3 +16,5 @@ print(f"Found {len(jobs)} jobs")
 print(jobs.head())
 
 jobs.to_csv("jobs.csv", quoting=csv.QUOTE_NONNUMERIC, escapechar="\\", index=False)
+# run our jsonnify.py to convert our csv to json
+subprocess.run(["python", "jsonify.py"])
