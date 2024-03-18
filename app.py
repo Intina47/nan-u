a9fa59f0699a1e5a0fb2d5f62683a4388f074c70
mamba <isaiahntina47@gmail.com>
2024-03-18 06:00:02 +0000

    Add prefix and load extensions to bot

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
 # path: app.py
+import asyncio
 import os
-from bot_components.bot import Nanéu
 import discord
+from discord.ext import commands
+from bot_components.bot import Nanéu
 
-discord_token = os.getenv('DISCORD_TOKEN')
+def get_prefix(bot, message):
+    prefix = f'<@!{bot.user.id}> !'
+    return commands.when_mentioned_or(prefix)(bot, message)
+
+async def load():
+    for filename in os.listdir('./cog'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cog.{filename[:-3]}')
+            print(f"Loaded {filename}.")
+        else:
+            print(f"Failed to load {filename}: not a .py file.")
+
 intents = discord.Intents.default()
-bot = Nanéu(intents=intents)
-bot.run(discord_token)
+intents.guilds = True
+intents.messages = True 
+intents.message_content = True
+bot = Nanéu(command_prefix=get_prefix, intents=intents)
+
+async def main():
+    await load()
+    discord_token = os.getenv('DISCORD_TOKEN')
+    await bot.start(discord_token)
+
+asyncio.run(main())
